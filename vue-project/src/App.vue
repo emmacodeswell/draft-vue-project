c2880bc0d197c357fb145312927d60c72eab8f7b
Emma Clarke <emmacodeswell@gmail.com>
2023-11-15 16:59:44 -0500

    Update `App.vue`

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -5,15 +5,11 @@ import tracker from './components/tracker.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    
   </header>
 
   <main>
-    <TheWelcome />
+    <tracker />
   </main>
 </template>
 
